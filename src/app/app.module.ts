66fc9549dd46e93c8031c54be0abb25b8a82fef0
mashudumelton <mashudumelton@gmail.com>
2018-08-29 14:33:24 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,39 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import{AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+var Firebaseconfig = {
+  apiKey: "AIzaSyDgNkUkuPGUuQoHPSq9GvY2esDKyEr0sh0",
+  authDomain: "firelist-c7a82.firebaseapp.com",
+  databaseURL: "https://firelist-c7a82.firebaseio.com",
+  projectId: "firelist-c7a82",
+  storageBucket: "firelist-c7a82.appspot.com",
+  messagingSenderId: "179263208493"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+ 
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(Firebaseconfig),
+    AngularFireDatabaseModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+  items: Observable<any[]>;
+name:string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private afDB:AngularFireDatabase) {
+    // this.items = afDB.list('cuisines').valueChanges();
+    this.items = this.afDB.list('cuisines').valueChanges();
+    console.log(this.items);
+  }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+  writeCuisines(){
+    this.afDB.list('cuisines').push(this.name);
   }
 
 }
